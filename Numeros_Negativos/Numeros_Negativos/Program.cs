148c4d6153be725f602a3c4844d29cf58066827d
Erick Adriano Nunes da Silva <61701606+ErickNunes97@users.noreply.github.com>
2022-03-04 21:02:24 -0300

    Add files via upload

---
+++ Numeros_Negativos/Numeros_Negativos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Numeros_Negativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nDigite um numero: \n");
+            int a = int.Parse(Console.ReadLine());
+
+            if( a < 0)
+            {
+                Console.WriteLine("\n" + a + " É um número NEGATIVO");
+            }
+
+            else
+            {
+                Console.WriteLine("\n" + a + " É um número POSITIVO");
+            }
+
+        }
+    }
+}
